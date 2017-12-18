1067058b16d9e56ba2e9694f5243de6dc4fc6525
maosilu <1540688711@qq.com>
2017-12-18 11:28:23 +0800

    no message

--- regex/regCheck.php
+++ regex/regCheck.php
@@ -7,14 +7,15 @@ require_once 'regexTool.class.php';
 $regex = new regexTool();
 $regex->setFixMode('U');
 $result = $regex->isMobile('13312345678');
-show($result);
+show($result, true);
 
-function show($var = null){
+function show($var = null, $isdump = false){
+	$func = $isdump ? 'var_dump' : 'print_r';
 	if(empty($var)){
 		echo 'null';
 	}elseif(is_array($var) || is_object($var)){
 		echo "<pre>";
-		print_r($var);
+		$func($var);
 	}else{
 		echo $var;
 	}
